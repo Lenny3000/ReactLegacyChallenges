697a699972d9429fadc0cf7f11858d0fa9ed01a9
Lenny3000 <91618917+Lenny3000@users.noreply.github.com>
2022-02-23 13:02:43 -0500

    challenge 3

--- src/Components/Cats/CatIndex.js
+++ src/Components/Cats/CatIndex.js
@@ -2,18 +2,28 @@ import React, { Component } from 'react';
 import CatList from './CatList';
  
 class CatIndex {
- constructor(){
-   super(props)
-   state : {
-     breeds: ['persian', 'siamese', 'maine coon', 'ragdoll', 'scottish fold', 'sphynx', 'british shorthair', 'bengal', 'american shorthair']
-   }
- }
-
-   return (
-     <div>
-      <CatList cats={breeds} />
-     </div>
-   );
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      breeds: [
+        "persian",
+        "siamese",
+        "maine coon",
+        "ragdoll",
+        "scottish fold",
+        "sphynx",
+        "british shorthair",
+        "bengal",
+        "american shorthair",
+      ],
+    };
+  }
+  render() {
+    return (
+      <div>
+        <CatList cats={breeds} />
+      </div>
+    );
+  }
 }
 
